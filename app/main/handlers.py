b0f8f67c7a2480cfe15698e02ee284b6c601a200
Kirill Stepankov <kirillstepankov18@gmail.com>
2023-03-18 23:50:28 +0300

    fix styling bugs

--- app/main/handlers.py
+++ app/main/handlers.py
@@ -1,10 +1,14 @@
 from app.main import bp
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask import render_template
+from app.main.forms import PostForm
 
 
 @bp.route('/')
 @bp.route('/index')
 @login_required
 def index():
-    return render_template('main/index.html', title='HAHAHA')
+    form = PostForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('main/index.html', title='HAHAHA', form=form)
