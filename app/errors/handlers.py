5fb515f8f04cc2110fc5d9bd895885193d979256
Kirill Stepankov <kirillstepankov18@gmail.com>
2023-03-20 00:00:43 +0300

    errors handling

---
+++ app/errors/handlers.py
@@ -0,0 +1,14 @@
+from app.errors import bp
+from flask import render_template
+from app import db
+
+
+@bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
+def page_not_found(e):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
